1bbde01f79628d8efc4e671d580ae0ef1c323a4d
pbzweihander <pbzweihander@gmail.com>
2023-05-31 14:54:26 +0900

    Install prettier and format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import logo from '/favicon.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import logo from "/favicon.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
