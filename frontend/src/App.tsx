6e32f84ee450a73e0c8e9e8ef735dd1b0f8777f0
pbzweihander <pbzweihander@gmail.com>
2023-05-31 16:00:25 +0900

    Implement basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,63 @@
 import { useState } from "react";
 
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import logo from "/favicon.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [chapter, setChapter] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={logo} className="logo" alt="Logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="navbar flex justify-center bg-slate-200 px-4">
+        <button
+          onClick={() => {
+            setChapter((chapter) => {
+              if (chapter <= 0) {
+                return 0;
+              } else {
+                return chapter - 1;
+              }
+            });
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <span className="mx-5 text-xl">Chapter {chapter + 1}</span>
+        <button
+          onClick={() => {
+            setChapter((chapter) => chapter + 1);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="flex h-[calc(100%-64px)] w-full">
+        <div className="h-full flex-1 bg-slate-100"></div>
+        <div className="h-full flex-1"></div>
+      </div>
     </>
   );
 }
